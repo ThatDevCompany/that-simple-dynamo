daa92c104ad31b810764c6366d1e19b7893ac902
Steve James <steve@thatdevcompany.com>
2019-02-18 11:30:21 +1100

    Latest Version

--- src/dynamoImpl/query.ts
+++ src/dynamoImpl/query.ts
@@ -12,14 +12,14 @@ import { DynamoError } from '@/errors/dynamo.error'
 export async function query<T extends M.IModel>(
 	c: AWS.DynamoDB.DocumentClient,
 	cls: M.IModelClass<T>,
-	query: O.IObjectStoreQueryOptions<T>
+	options: O.IObjectStoreQueryOptions<T>
 ): Promise<O.IObjectStoreQueryResult<T>> {
 	// Attempt Query from Dynamo
 	try {
-		query = query || {}
+		options = options || {}
 
 		// Build the Query
-		const keys: Array<string> = _.keys(query.where || {})
+		const keys: Array<string> = _.keys(options.where || {})
 		const dynamoQuery = {
 			TableName: DynamoUtils.getTableName(cls),
 			...(where => {
@@ -33,28 +33,44 @@ export async function query<T extends M.IModel>(
 					FilterExpression: condition,
 					ExpressionAttributeNames: names,
 					ExpressionAttributeValues: values,
-					ExclusiveStartKey: query.continuationToken
+					ExclusiveStartKey: options.continuationToken
 				}
-			})(query.where)
+			})(options.where)
 		}
 
 		// Create and Run DynamoDB Query
 		return await util
 			.promisify(c.scan.bind(c))(dynamoQuery)
 			// Convert results into their Model classes
-			.then(({ Items, LastEvaluatedKey }) => {
-				return {
-					items: (Items || /* istanbul ignore next */ []).map(Item =>
+			.then(async ({ Items, LastEvaluatedKey }) => {
+				let items = (Items || []).map(Item =>
 						DynamoUtils.dynamoToClass(cls, Item)
 					),
-					continuationToken: LastEvaluatedKey
+					continuationToken = LastEvaluatedKey
+
+				// IF there are more results to get
+				if (
+					(!options.continuationCount || options.continuationCount > 0) &&
+					continuationToken
+				) {
+					const res = await query<T>(c, cls, {
+						...options,
+						continuationToken: continuationToken,
+						continuationCount: (options.continuationCount || 10) - 1
+					})
+					return {
+						items: [].concat(items, res.items),
+						continuationToken: res.continuationToken
+					}
 				}
+				// OTHERWISE
+				return { items, continuationToken }
 			})
 			// Perform any additionality filtering specified as part of the query
 			.then(data =>
-				query.filter
+				options.filter
 					? {
-							items: data.items.filter(query.filter),
+							items: data.items.filter(options.filter),
 							continuationToken: data.continuationToken
 					  }
 					: data
